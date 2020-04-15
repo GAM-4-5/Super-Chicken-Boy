9c4043e4cde19e69e315ae43a0812829978e0c75
Vjekoslav194 <62606854+Vjekoslav194@users.noreply.github.com>
2020-04-15 16:50:56 +0200

    Update igrica.py

--- Super Chicken Boy/igrica.py
+++ Super Chicken Boy/igrica.py
@@ -3,7 +3,7 @@ pygame.init()
 
 win = pygame.display.set_mode((600,400))
 
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("Super_Chicken_Boy")
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
