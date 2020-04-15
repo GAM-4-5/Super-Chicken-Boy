fd1468b1ba5fa484276cf61eb0f68f1877c684cb
Vjekoslav194 <62606854+Vjekoslav194@users.noreply.github.com>
2020-04-15 17:09:08 +0200

    Update igrica.py

--- Super Chicken Boy/igrica.py
+++ Super Chicken Boy/igrica.py
@@ -1,19 +1,36 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((600,400))
+win = pygame.display.set_mode((600,400)) ##predispozicije prozora
 
-pygame.display.set_caption("Super_Chicken_Boy")
+pygame.display.set_caption("Super_Chicken_Boy") ##ime tj. naslov prikazan na prozoru
 
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
-bg = pygame.image.load('bg2.jpg')
-char = pygame.image.load('standing.png')
+walkRight = [pygame.image.load('R1.png'), ##slike koje se mijenjaju kad je dana naredba za kretanje u desno
+             pygame.image.load('R2.png'),
+             pygame.image.load('R3.png'),
+             pygame.image.load('R4.png'),
+             pygame.image.load('R5.png'),
+             pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'),
+             pygame.image.load('R8.png'),
+             pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), ##slike koje se mijenjaju kad je dana naredba za kretanje u desno
+            pygame.image.load('L2.png'),
+            pygame.image.load('L3.png'),
+            pygame.image.load('L4.png'),
+            pygame.image.load('L5.png'),
+            pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), 
+            pygame.image.load('L8.png'),
+            pygame.image.load('L9.png')]
+
+bg = pygame.image.load('bg2.jpg') ##pozadinska slika,fiksna je
+char = pygame.image.load('standing.png') ##slika lika na početku prije davanja komandi za kretanje
 
 clock = pygame.time.Clock()
 
 
-class player(object):
+class player(object): ##atributi lika (početna pozicija, brzina itd.)
     def __init__(self,x,y,width,height):
         self.x = x
         self.y = y
@@ -31,14 +48,14 @@ class player(object):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
 
-        if not(self.standing):
+        if not(self.standing): ##komande ako je lik u pokretu
             if self.left:
                 win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
                 self.walkCount += 1
             elif self.right:
                 win.blit(walkRight[self.walkCount//3], (self.x,self.y))
                 self.walkCount +=1
-        else:
+        else:                  ##ako se prestao gibati koja će slika ostati ovisno o zadnjem smjeru kretanja
             if self.right:
                 win.blit(walkRight[0], (self.x, self.y))
             else:
@@ -46,7 +63,7 @@ class player(object):
                 
 
 
-class projectile(object):
+class projectile(object): ##atributi metka
     def __init__(self,x,y,radius,color,facing):
         self.x = x
         self.y = y
@@ -55,7 +72,7 @@ class projectile(object):
         self.facing = facing
         self.vel = 10 * facing
 
-    def draw(self,win):
+    def draw(self,win): ##crtanje metka
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
 
 
@@ -66,43 +83,43 @@ def redrawGameWindow():
     for bullet in bullets:
         bullet.draw(win)
     
-    pygame.display.update()
+    pygame.display.update() ##osvježavanje prozora da prikazuje elemente na ekranu
 
 
-#mainloop
+##glavna petlja, tu su sve radnje koje se trenutno izvode
 man = player(50, 320, 64,64)
-bullets = []
+bullets = [] ##prazna lista u kojoj su poslije metci 
 run = True
 while run:
     clock.tick(27)
 
-    for event in pygame.event.get():
+    for event in pygame.event.get(): ##zatvaranje prozora ako se stisne X
         if event.type == pygame.QUIT:
             run = False
         
     for bullet in bullets:
-        if bullet.x < 500 and bullet.x > 0:
+        if bullet.x < 600 and bullet.x > 0: ##metak smije ispalit ako je u granicama prozora
             bullet.x += bullet.vel
         else:
-            bullets.pop(bullets.index(bullet))
+            bullets.pop(bullets.index(bullet)) ##ako metak izađe van ekrana onda mora nestat
 
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_SPACE]:
         if man.left:
-            facing = -1
+            facing = -1 ##ako ide u lijevo onda mora oduzet
         else:
-            facing = 1
+            facing = 1 ##ako ide u desno onda dodaje prema x osi
             
-        if len(bullets) < 8:
+        if len(bullets) < 8: ##broj metaka koji maksimalno može bit na ekranu
             bullets.append(projectile(round(man.x + man.width //2), round(man.y + man.height//2), 9, (255,0,0), facing))
 
-    if keys[pygame.K_LEFT] and man.x > man.vel:
+    if keys[pygame.K_LEFT] and man.x > man.vel: ##kretanje po x osi u lijevo, ovisi o brzini koja je zadana
         man.x -= man.vel
         man.left = True
         man.right = False
         man.standing = False
-    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel: ##kretanje po x osi u desno, ovisi o brzini koja je zadana
         man.x += man.vel
         man.right = True
         man.left = False
@@ -122,7 +139,7 @@ while run:
             neg = 1
             if man.jumpCount < 0:
                 neg = -1
-            man.y -= (man.jumpCount ** 2) * 0.5 * neg
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg ##kvadratna funkcija, skok je prezentiran kao parabola
             man.jumpCount -= 1
         else:
             man.isJump = False
